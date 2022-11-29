c8cf81e3af9b3f94c89b2757f982275236bc5513
slimmjimmy <jakefalagrady@gmail.com>
2022-11-29 09:31:43 -0700

    fixed views

--- INTEX2022/mainapp/views.py
+++ INTEX2022/mainapp/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexPageView(request) :
-    return render( request, 'homepages/index.html')
+    return render( request, 'index.html')
 
 def journalPageView(request) :
-    return render( request, 'homepages/index.html')
+    return render( request, 'journal.html')
 
 def loginPageView(request) :
-    return render( request, 'homepages/index.html')
+    return render( request, 'login.html')
 
 def profilePageView(request) :
-    return render( request, 'homepages/index.html')
+    return render( request, 'profile.html')
 
 def createuserPageView(request) :
-    return render( request, 'homepages/index.html')
+    return render( request, 'createuser.html')
 
 def dashboardPageView(request) :
-    return render( request, 'homepages/index.html')
+    return render( request, 'dashboard.html')
